7bf23883e07baed97b1e0828edb7ea98c54d09bd
Alex Dingal <lhexie2014@gmail.com>
2022-12-18 20:04:26 +0800

    [Update] Add select change handler

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect } from 'react'
 import CustomContainer from 'atoms/CustomContainer'
+import BreedCard from 'atoms/BreedCard'
 
 import CatContext from 'context/CatContext'
-import { getCatBreeds } from 'context/CatActions'
+import { getCatBreeds, getSelectedBreed } from 'context/CatActions'
 
 const Content = () => {
-  const { breeds, dispatch } = useContext(CatContext)
+  const { breed, breeds, dispatch } = useContext(CatContext)
 
   useEffect(() => {
     dispatch({ type: 'SET_LOADING' })
@@ -16,33 +17,45 @@ const Content = () => {
     getBreeds()
   }, [dispatch])
 
+  const handleSelectChange = async (e) => {
+    dispatch({ type: 'SET_LOADING' })
+    const getBreed = await getSelectedBreed(e.target.value, 1)
+    dispatch({ type: 'GET_SELECTED_BREED', payload: getBreed })
+  }
+
+  const renderSelect = () => (
+    <select
+      onChange={handleSelectChange}
+      className="w-full min-w-[150px] text-sm text-gray-700 border rounded-md p-2 outline-none cursor-pointer md:w-auto md:text-base"
+      defaultValue="default"
+    >
+      <option value="default" disabled>
+        Choose breed
+      </option>
+      {breeds?.length > 0 &&
+        breeds.map((breed) => (
+          <option value={breed.id} key={breed.id}>
+            {breed.name}
+          </option>
+        ))}
+    </select>
+  )
+
   return (
     <main className="mb-auto py-8 lg:py-16">
       <CustomContainer>
-        <div className="mb-4 lg:mb-6">
-          <select
-            className="w-full min-w-[150px] text-sm text-gray-700 border rounded-md p-2 outline-none cursor-pointer md:w-auto md:text-base"
-            defaultValue="default"
-          >
-            <option value="default" disabled>
-              Choose breed
-            </option>
-            {breeds?.length > 1 &&
-              breeds.map((breed) => (
-                <option value={breed.id} key={breed.id}>
-                  {breed.name}
-                </option>
-              ))}
-          </select>
-        </div>
-        <div className="grid grid-cols-1 gap-y-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-4">
-          <div className="w-full">
-            <img src="https://www.dummyimage.com/400x300" alt="Dummy" />
-            <p className="text-sm text-center py-2 border md:text-base">
-              <a href="/cat-id">Cat Breed</a>
-            </p>
+        <div className="mb-4 lg:mb-6">{renderSelect()}</div>
+        {breed?.length > 0 ? (
+          <div className="grid grid-cols-1 gap-y-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-4">
+            {breed.map((item) => (
+              <BreedCard item={item} key={item.id} />
+            ))}
           </div>
-        </div>
+        ) : (
+          <div>
+            <p>Please select cat breed.</p>
+          </div>
+        )}
       </CustomContainer>
     </main>
   )
