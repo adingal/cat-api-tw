5244cca1633121560935fcbefb05d2ecaa4d8712
Alex Dingal <lhexie2014@gmail.com>
2022-12-21 01:22:10 +0800

    [Update] Add states and functions from Content component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import 'assets/css/style.css'
 
@@ -8,22 +8,56 @@ import Content from 'components/Content'
 import CatInfo from 'components/CatInfo'
 import Footer from 'components/Footer'
 
-import { CatProvider } from 'context/CatContext'
+import CatContext from 'context/CatContext'
+import { getCatBreeds, getSelectedBreed } from 'context/CatActions'
 
 const App = () => {
+  const { breeds, currentBreed, dispatch } = useContext(CatContext)
+  const [page, setPage] = useState(1)
+
+  // Get breeds on initial render to fill select input
+  useEffect(() => {
+    if (breeds?.length === 0) {
+      dispatch({ type: 'SET_LOADING' })
+      const getBreeds = async () => {
+        const catBreeds = await getCatBreeds()
+        dispatch({ type: 'GET_BREEDS', payload: catBreeds })
+      }
+      getBreeds()
+    }
+  }, [dispatch])
+
+  // Get current breed when page is updated
+  useEffect(() => {
+    if (currentBreed !== '') {
+      dispatch({ type: 'SET_LOADING' })
+      getBreed(currentBreed, page)
+    }
+  }, [page])
+
+  // Get  breed
+  const getBreed = async (value, page) => {
+    dispatch({ type: 'SET_LOADING' })
+    const data = await getSelectedBreed(value, page)
+    dispatch({ type: 'GET_SELECTED_BREED', payload: data })
+  }
+
   return (
-    <CatProvider>
-      <Layout>
-        <Header />
-        <Router>
-          <Routes>
-            <Route path="/" element={<Content />} />
-            <Route path="/:id" element={<CatInfo />} />
-          </Routes>
-        </Router>
-        <Footer />
-      </Layout>
-    </CatProvider>
+    <Layout>
+      <Header />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Content page={page} setPage={setPage} getBreed={getBreed} />
+            }
+          />
+          <Route path="/:id" element={<CatInfo />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </Layout>
   )
 }
 
